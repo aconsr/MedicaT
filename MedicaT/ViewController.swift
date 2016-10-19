ab0ac145d474d26bbf745a0bb47a9dca734b14cc
Alan Valdez <agvaldezc@Alans-MacBook-Pro.local>
2016-10-19 15:18:05 -0500

    Initial Commit

---
+++ MedicaT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedicaT
+//
+//  Created by Alan Valdez on 10/19/16.
+//  Copyright © 2016 ITESM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
